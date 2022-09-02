d517b30269b24761ad3703e73a779e6253aca325
bladeshape <tekigod@mail.ru>
2022-09-02 04:05:20 +0300

    Ex47dn

--- Ex47/Program.cs
+++ Ex47/Program.cs
@@ -1,9 +1,38 @@
 ﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-
+// Например:
 // m = 3, n = 4.
-
 // 0,5 7 -2 -0,2
-
 // 1 -3,3 8 -9,9
-
 // 8 7,8 -7,1 9
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Количество строк = {m}, Количество столбцов = {n}.");
+double[,] numbers = new double[m, n];
+FillMatrixRandomNumbers(numbers);
+PrintMatrix(numbers);
+
+void FillMatrixRandomNumbers(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}    ");
+        }
+        Console.WriteLine("");
+    }
+}
