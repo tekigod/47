410ebbee0190e93d1dc2db4e1f3437a5687dfe85
bladeshape <tekigod@mail.ru>
2022-09-02 05:30:23 +0300

    Ex52d

--- Ex47/Program.cs
+++ Ex47/Program.cs
@@ -29,10 +29,12 @@ void PrintMatrix(double[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
+        Console.Write("[  ");
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
             Console.Write($"{matrix[i, j]}    ");
         }
+        Console.Write("]");
         Console.WriteLine("");
     }
 }
